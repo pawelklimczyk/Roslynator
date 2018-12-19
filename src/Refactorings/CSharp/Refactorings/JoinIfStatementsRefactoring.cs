61c2f2022312ea194f2d4bb46973c0a82dc3bdd9
Josef Pihrt <josef.pihrt@outlook.com>
2018-12-19 18:03:23 +0100

    Add refactoring JoinIfStatements.

---
+++ src/Refactorings/CSharp/Refactorings/JoinIfStatementsRefactoring.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Roslynator.CSharp.Refactorings
+{
+    internal static class JoinIfStatementsRefactoring
+    {
+        public static void ComputeRefactorings(RefactoringContext context, StatementListSelection selectedStatements)
+        {
+            if (selectedStatements.Count < 2)
+                return;
+
+            for (int i = 0; i < selectedStatements.Count; i++)
+            {
+                if (!(selectedStatements[i] is IfStatementSyntax ifStatement))
+                    return;
+
+                foreach (IfStatementOrElseClause ifOrElse in ifStatement.AsCascade())
+                {
+                    if (ifOrElse.IsElse)
+                        return;
+
+                    StatementSyntax statement = ifOrElse.Statement;
+
+                    if (statement is BlockSyntax block)
+                        statement = block.Statements.LastOrDefault();
+
+                    if (statement == null)
+                        return;
+
+                    if (!statement.IsKind(
+                        SyntaxKind.ReturnStatement,
+                        SyntaxKind.ContinueStatement,
+                        SyntaxKind.BreakStatement,
+                        SyntaxKind.ThrowStatement))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            Document document = context.Document;
+
+            context.RegisterRefactoring(
+                "Join if statements",
+                ct => RefactorAsync(document, selectedStatements, ct),
+                RefactoringIdentifiers.JoinIfStatements);
+        }
+
+        private static Task<Document> RefactorAsync(
+            Document document,
+            StatementListSelection selectedStatements,
+            CancellationToken cancellationToken)
+        {
+            var ifStatement = (IfStatementSyntax)selectedStatements.Last();
+
+            for (int i = selectedStatements.Count - 2; i >= 0; i--)
+            {
+                var ifStatement2 = (IfStatementSyntax)selectedStatements[i];
+
+                IfStatementSyntax lastIf = ifStatement2.GetCascadeInfo().Last.AsIf();
+
+                IfStatementSyntax newLastIf = lastIf.WithElse(SyntaxFactory.ElseClause(ifStatement));
+
+                ifStatement = ifStatement2.ReplaceNode(lastIf, newLastIf);
+            }
+
+            SyntaxList<StatementSyntax> newStatements = selectedStatements.UnderlyingList
+                .Replace(selectedStatements.First(), ifStatement.WithFormatterAnnotation())
+                .RemoveRange(selectedStatements.FirstIndex + 1, selectedStatements.Count - 1);
+
+            return document.ReplaceStatementsAsync(SyntaxInfo.StatementListInfo(selectedStatements), newStatements, cancellationToken);
+        }
+    }
+}
---
+++ src/Tests/Refactorings.Tests/RRX007JoinIfStatementsTests.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Roslynator.CSharp.Refactorings.Tests
+{
+    public class RRX007JoinIfStatementsTests : AbstractCSharpCodeRefactoringVerifier
+    {
+        public override string RefactoringId { get; } = RefactoringIdentifiers.JoinIfStatements;
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.JoinIfStatements)]
+        public async Task Test()
+        {
+            await VerifyRefactoringAsync(@"
+class C
+{
+    int M()
+    {
+        bool f1 = false;
+
+[|        if (f1)
+            return 1;
+
+        if (f1)
+        {
+            return 2;
+        }
+        else if (f1)
+        {
+            return 3;
+        }
+
+        if (f1)
+        {
+            return 4;
+        }|]
+
+        return 0;
+    }
+}
+", @"
+class C
+{
+    int M()
+    {
+        bool f1 = false;
+
+        if (f1)
+            return 1;
+        else if (f1)
+        {
+            return 2;
+        }
+        else if (f1)
+        {
+            return 3;
+        }
+        else if (f1)
+        {
+            return 4;
+        }
+
+        return 0;
+    }
+}
+", equivalenceKey: RefactoringId);
+        }
+    }
+}
