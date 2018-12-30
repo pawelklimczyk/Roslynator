61c11026be68356c60bd3fc0496c7abfc95e9bae
Josef Pihrt <josef.pihrt@outlook.com>
2018-12-30 15:01:50 +0100

    x

--- src/Common.Workspaces/CSharp/CodeActionFactory.cs
+++ src/Common.Workspaces/CSharp/CodeActionFactory.cs
@@ -5,6 +5,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslynator.CSharp.Refactorings;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Roslynator.CSharp
 {
@@ -30,7 +31,20 @@ namespace Roslynator.CSharp
             string title = null,
             string equivalenceKey = null)
         {
-            title = title ?? $"Change type to '{SymbolDisplay.ToMinimalDisplayString(newTypeSymbol, semanticModel, type.SpanStart)}'";
+            if (title == null)
+            {
+                string newTypeName = SymbolDisplay.ToMinimalDisplayString(newTypeSymbol, semanticModel, type.SpanStart);
+
+                if ((type.Parent is MethodDeclarationSyntax methodDeclaration && methodDeclaration.ReturnType == type)
+                    || (type.Parent is LocalFunctionStatementSyntax localFunction && localFunction.ReturnType == type))
+                {
+                    title = $"Change return type to '{newTypeName}'";
+                }
+                else
+                {
+                    title = $"Change type to '{newTypeName}'";
+                }
+            }
 
             return ChangeType(document, type, newTypeSymbol, title, equivalenceKey);
         }
@@ -45,6 +59,68 @@ namespace Roslynator.CSharp
             return CodeAction.Create(
                 title,
                 ct => ChangeTypeRefactoring.ChangeTypeAsync(document, type, newTypeSymbol, ct),
+                equivalenceKey);
+        }
+
+        public static CodeAction AddCastExpression(
+            Document document,
+            ExpressionSyntax expression,
+            ITypeSymbol destinationType,
+            SemanticModel semanticModel,
+            string title = null,
+            string equivalenceKey = null)
+        {
+            string typeName = SymbolDisplay.ToMinimalDisplayString(destinationType, semanticModel, expression.SpanStart);
+
+            TypeSyntax newType = ParseTypeName(typeName);
+
+            return CodeAction.Create(
+                title ?? $"Cast to '{typeName}'",
+                ct => AddCastExpressionRefactoring.RefactorAsync(document, expression, newType, ct),
+                equivalenceKey);
+        }
+
+        public static CodeAction RemoveMemberDeclaration(
+            Document document,
+            MemberDeclarationSyntax memberDeclaration,
+            string title = null,
+            string equivalenceKey = null)
+        {
+            return CodeAction.Create(
+                title ?? $"Remove {CSharpFacts.GetTitle(memberDeclaration)}",
+                ct => document.RemoveMemberAsync(memberDeclaration, ct),
+                equivalenceKey);
+        }
+
+        public static CodeAction RemoveStatement(
+            Document document,
+            StatementSyntax statement,
+            string title = null,
+            string equivalenceKey = null)
+        {
+            return CodeAction.Create(
+                title ?? $"Remove {CSharpFacts.GetTitle(statement)}",
+                ct => document.RemoveStatementAsync(statement, ct),
+                equivalenceKey);
+        }
+
+        public static CodeAction ReplaceNullWithDefaultValue(
+            Document document,
+            ExpressionSyntax expression,
+            ITypeSymbol typeSymbol,
+            string title = null,
+            string equivalenceKey = null)
+        {
+            return CodeAction.Create(
+                title ?? "Replace 'null' with default value",
+                ct =>
+                {
+                    ExpressionSyntax defaultValue = typeSymbol
+                        .GetDefaultValueSyntax(document.GetDefaultSyntaxOptions())
+                        .WithTriviaFrom(expression);
+
+                    return document.ReplaceNodeAsync(expression, defaultValue, ct);
+                },
                 equivalenceKey);
         }
     }
